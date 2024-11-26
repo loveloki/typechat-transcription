8a279068bb5c56d5c699fd2cce99823e01253cf3
xleine <xleine@qq.com>
2024-11-27 01:35:35 +0800

    feat: no-unused-vars eslint ignore ^_

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -43,6 +43,18 @@ const customConfig = {
         next: ["import"],
       },
     ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
   },
 }
 
